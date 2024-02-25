a2d2c9a09e78d080261d3d94a88de95346e6dbdc
PierreTvt <100429263+PierreTvt@users.noreply.github.com>
2024-02-25 15:30:25 +0100

    Ajout d'un fichier, contenant un debut de classe Paquet. De plus, des parametres ont ete modifié afin d'avoir un affichage console

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Paquet
+{
+	int[,] tabPaquet = new int[52, 2]; // plutot utiliser List<xxxx>, l'ideal serait une liste de tableau
+	public Paquet()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+		initPaquet(); // initialise le paquet
+	}
+	public void initPaquet()
+	{
+		// A changer lorsque tabPaquet sera une List (en utilisant la méthode add)
+		for (int couleur=0; couleur<3; couleur++)
+		{
+			for (int i=1; i<14; i++)
+			{
+				tabPaquet[(i - 1) * (couleur + 1), 0] = i;
+				tabPaquet[(i - 1) * (couleur + 1), 1] = couleur;
+			}
+		}
+	}
+	public int[] piocheCarte(int num)
+	{
+		// il faudrait aussi supprimer la carte pioché
+		return  new int[] { tabPaquet[num, 0], tabPaquet[num, 1]}; // renvoie la premiere carte sans la supprimer
+	}
+	public int[] piocheCarteAlea()
+	{
+		// retourne une carte du paquet aléatoirement, et la supprime
+		// cette méthode peut appeler piocheCarte(num), ou non (a voir le plus simple et le plus clair)
+		return piocheCarte(0);
+	}
+}
+
+
+
+
+public class PokerTextuel
+{
+	public void run()
+	{
+        Console.WriteLine("Debut fonction run");
+        test();
+	}
+	public void test()
+	{
+		Console.WriteLine("Debut fonction test");
+        // fonction pour faire des tests
+        Paquet paquet = new Paquet();
+		int[] carte = paquet.piocheCarte(0);
+		Console.WriteLine("carte:"+carte[0]+" " + carte[1]);
+        Console.ReadLine();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
 
 // instanciation de la classe game, puis lancement de cette classe
-using var game = new testJeuxCross2.Game1();
-game.Run();
+using System; // a retirer possiblement apres tout les tests consoles
+
+//using var game = new testJeuxCross2.Game1();
+//game.Run();
+
+//test en console:
+Console.WriteLine("debut programme Programm.cs");
+PokerTextuel gameText = new PokerTextuel();
+gameText.run();
+
+// probleme possible avec obj\project.assets.json
+// il faut utiliser dotnet restore (demande a Pierre si besoin)
+
+// pour que l'execution fonctionne, j'ai selectionné testJeuxCross2 a coté du triangle vert
+
+/* /!\ IMPORTANT /!\ (Pierre THEVENOT)
+ * Pour executer le programme avec une sortie console, j'ai du changer une ligne du fichier testJeuxCross2.csproj
+ * J'ai changé (uniquement !) la 3eme ligne (commencant par <OutputType>...)
+ * J'ai mis (sans les doubles guillemets):
+"    <OutputType>Exe</OutputType>"
+ * avant, la ligne était (très exactement, mais sans les doubles guillemets):
+"    <OutputType>WinExe</OutputType>"
+ * Quand les tests en consoles seront terminé, 
+ * il FAUDRA pour le bon fonctionnement du programme remettre la lignes comme avant.
+*/
